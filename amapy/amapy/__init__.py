34adb80fe0fbd5d6b805ff72e0d02b777d5a989d
julienvitard <julienvitard@gmail.com>
2015-05-25 14:50:26 +0200

    Adds some project's infos (versions, author, email, ...)

---
+++ amapy/amapy/__init__.py
@@ -0,0 +1,11 @@
+"""
+Amapy:
+"""
+
+__version__ = '0.0.0'
+__package_name__ = "amapy"
+__author__ = "Julien Vitard"
+__author_email__ = "julienvitard+amapy@gmail.com"
+__url__ = "https://github.com/julienvitard/amapy"
+
+VERSION = tuple(__version__.split('.'))
