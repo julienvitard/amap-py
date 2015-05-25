bc0b2e47442792efc0209f525deef81dce683c63
julienvitard <julienvitard@gmail.com>
2015-05-25 15:11:10 +0200

    Adds import first tests using pytest

---
+++ amapy/amapy/tests/test_imports.py
@@ -0,0 +1,28 @@
+import pytest
+
+
+class TestImports:
+
+    def test_version(self):
+        from amapy import __version__
+        assert __version__ == '0.0.0'
+
+    def test_package_name(self):
+        from amapy import __package_name__
+        assert __package_name__ == "amapy"
+
+    def test_author(self):
+        from amapy import __author__
+        assert __author__ == "Julien Vitard"
+
+    def test_author_email(self):
+        from amapy import __author_email__
+        assert __author_email__ == "julienvitard+amapy@gmail.com"
+
+    def test_url(self):
+        from amapy import __url__
+        assert __url__ == "https://github.com/julienvitard/amapy"
+
+
+if __name__ == '__main__':
+    pytest.main()
