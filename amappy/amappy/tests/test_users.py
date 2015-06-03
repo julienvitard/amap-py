25612d3240f3b5e2b181127a15441e60b2526c23
julienvitard <julienvitard@gmail.com>
2015-06-03 02:08:11 +0200

    Adds deny delete without credential test

--- amappy/amappy/tests/test_users.py
+++ amappy/amappy/tests/test_users.py
@@ -3,12 +3,17 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from amappy.users.models import User
 
+from rest_framework import status
+
 
 test_attributes = {
-    "username":   "uset_test",
-    "email":      "user_test+amappy@gmail.com",
-    "first_name": "User",
-    "last_name":  "Test",
+    "username":       "uset_test",
+    "email":          "user_test+amappy@gmail.com",
+    "first_name":     "User",
+    "last_name":      "Test",
+    "phone_number":   "6666666666",
+    "is_distributor": False,
+    "is_supervisor":  False,
 }
 
 
@@ -31,7 +36,7 @@ class UserModelTest(TestCase):
 
     def test_users_list_view(self):
         empty_resp = self.client.get('/users/')
-        assert empty_resp.status_code == 200
+        assert empty_resp.status_code == status.HTTP_200_OK
         assert empty_resp.data == []
 
         self.model.objects.create(**test_attributes)
@@ -44,3 +49,9 @@ class UserModelTest(TestCase):
         user = resp.data
         for key, value in test_attributes.items():
             assert user.get(key) == value
+
+    def test_users_deny_delete(self):
+        self.model.objects.create(**test_attributes)
+        # no credentials to delete user
+        resp = self.client.delete('/users/1/')
+        assert resp.status_code == status.HTTP_403_FORBIDDEN
