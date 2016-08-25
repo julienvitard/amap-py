df2f5366e7a2798296df367002409f88c426dd3c
Julien Vitard <julienvitard@gmail.com>
2016-08-25 23:46:26 +0200

    add InMemoryDB, init test

--- amappy/amappy/persistence/base.py
+++ amappy/amappy/persistence/base.py
@@ -26,3 +26,29 @@ class Persistence:
     @classmethod
     def reset(cls):
         raise NotImplementedError
+
+
+class InMemoryDB(Persistence):
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def create(cls, data=None):
+        pass
+
+    @classmethod
+    def read(cls, key=None):
+        pass
+
+    @classmethod
+    def update(cls, key=None, data=None):
+        pass
+
+    @classmethod
+    def delete(cls, key=None):
+        pass
+
+    @classmethod
+    def reset(cls):
+        pass
