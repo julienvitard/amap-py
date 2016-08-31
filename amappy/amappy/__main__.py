f7f8b42f1bbe8236f5e9b934567596939d2177e4
Julien Vitard <julienvitard@gmail.com>
2016-09-01 00:08:23 +0200

    add port/host to REST API cli launcher

--- amappy/amappy/__main__.py
+++ amappy/amappy/__main__.py
@@ -4,15 +4,29 @@
 """REST API launcher
 """
 
+import argparse
 import os
 import sys
 
 
-def main():
+def main(args):
     """The main routine."""
     from amappy.api.rest import app
-    app.run()
+    app.run(host=args.host, port=args.port)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description='amappy REST API launcher'
+    )
+    parser.add_argument(
+        '--host', metavar='HOST', type=str, default="127.0.0.1",
+        help='host ip to connect to'
+    )
+    parser.add_argument(
+        '--port', dest='port', type=int, default=8000,
+        help='port to connect to'
+    )
+
+    args = parser.parse_args()
+    main(args)
